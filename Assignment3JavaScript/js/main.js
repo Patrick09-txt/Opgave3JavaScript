464c88a2a12a0ed36bccbde2f6e8a53bffda5d18
Patrick Nyskjold <74715124+Patrick09-txt@users.noreply.github.com>
2021-09-26 18:14:30 +0200

    Add files via upload

---
+++ Assignment3JavaScript/js/main.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            word: null,
+            words: [],
+            message: null,
+            message1: null,
+            message2: null
+        }
+    },
+    methods: {
+        show(word) {
+            this.words.push(word)
+
+            if (this.words == null || this.words.length == 0) {
+                this.message = "empty"
+            }
+            else {
+                this.message = this.words.toString()
+                this.message1 = this.words.toString().toUpperCase()
+                this.message2 = this.words.toString().toLowerCase()
+            }
+        }
+    }
+}).mount("#app")
